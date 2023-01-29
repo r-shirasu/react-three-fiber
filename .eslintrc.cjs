aebc67c8e642cd23962c7a9a0660dc6725dc9792
r-shirasu <risako.shirasu@asia-quest.jp>
2023-01-29 23:49:37 +0900

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,8 @@ module.exports = {
         "es2022": true
     },
     "extends": [
-        'airbnb',
-        'airbnb-typescript',
-        'airbnb/hooks',
         "eslint:recommended",
-        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
         "plugin:@typescript-eslint/recommended",
         'prettier'
     ],
@@ -24,12 +21,5 @@ module.exports = {
     ],
     "rules": {
         'no-console': ['error'],
-        'react/react-in-jsx-scope': 'off',
-        "react/function-component-definition": [
-            2,
-            { "namedComponents": ['function-declaration', 'arrow-function'] }
-        ],
-        "import/prefer-default-export": "off",
-        '@typescript-eslint/no-shadow': 'off',
     }
 }
