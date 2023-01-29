d566e3cc710b69b16fd670884b4296aa032c7bca
r-shirasu <risako.shirasu@asia-quest.jp>
2023-01-30 00:52:30 +0900

    feat: カスタムジオメトリの作成

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,6 +1,7 @@
-import {useFrame} from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import { Group, Mesh } from 'three';
+import CustomObject from './CustomObject';
 
 export default function Experience() {
   const cubeRef = useRef<Mesh>(null);
@@ -21,24 +22,25 @@ export default function Experience() {
     group.rotation.y += delta;
   });
   return (
-      <>
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 }/>
-        <ambientLight intensity={ 0.5 } />
+    <>
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <group ref={groupRef}>
-          <mesh position-x={-2}>
-            <sphereGeometry/>
-            <meshStandardMaterial color="orange"/>
-          </mesh>
-          <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-            <boxGeometry/>
-            <meshStandardMaterial color="mediumpurple"/>
-          </mesh>
-        </group>
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-          <planeGeometry/>
-          <meshStandardMaterial color="greenyellow"/>
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
         </mesh>
-      </>
+        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      <CustomObject />
+    </>
   );
 }
