d4d8df23b5ffb3f3b41f099116c15bb2535ae2e5
r-shirasu <risako.shirasu@asia-quest.jp>
2023-01-30 00:36:27 +0900

    feat: 光の操作

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,15 +1,10 @@
-import {useFrame, extend, useThree} from '@react-three/fiber'
+import {useFrame} from '@react-three/fiber'
 import { useRef } from 'react';
 import { Group, Mesh } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-extend({ OrbitControls })
 
 export default function Experience() {
   const cubeRef = useRef<Mesh>(null);
   const groupRef = useRef<Group>(null);
-
-  const {camera, gl} = useThree();
 
   useFrame((state, delta) => {
     const cube = cubeRef.current;
@@ -27,21 +22,22 @@ export default function Experience() {
   });
   return (
       <>
-        <orbitControls args={ [ camera, gl.domElement ] } />
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 }/>
+        <ambientLight intensity={ 0.5 } />
 
         <group ref={groupRef}>
           <mesh position-x={-2}>
             <sphereGeometry/>
-            <meshBasicMaterial color="orange"/>
+            <meshStandardMaterial color="orange"/>
           </mesh>
           <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
             <boxGeometry/>
-            <meshBasicMaterial color="mediumpurple"/>
+            <meshStandardMaterial color="mediumpurple"/>
           </mesh>
         </group>
         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
           <planeGeometry/>
-          <meshBasicMaterial color="greenyellow"/>
+          <meshStandardMaterial color="greenyellow"/>
         </mesh>
       </>
   );
