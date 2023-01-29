495c544a31b679100f26f5f5a3a1e4524f71dccf
r-shirasu <risako.shirasu@asia-quest.jp>
2023-01-30 01:10:16 +0900

    feat: 最初のR3Fアプリケーション終わりまで

--- src/Three.tsx
+++ src/Three.tsx
@@ -1,9 +1,23 @@
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience';
+import * as THREE from 'three';
 
 export const Three = () => {
   return (
-    <Canvas>
+    <Canvas
+      dpr={[1, 2]}
+      gl={{
+        antialias: true,
+        toneMapping: THREE.ACESFilmicToneMapping,
+        outputEncoding: THREE.sRGBEncoding,
+      }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [3, 2, 6],
+      }}
+    >
       <Experience />
     </Canvas>
   );
