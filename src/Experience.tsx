f5d805104f6f82320dacc5e1afffde3c1e33d2c1
r-shirasu <risako.shirasu@asia-quest.jp>
2023-01-30 23:57:20 +0900

    feat: PivotControls

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,22 +1,37 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PivotControls, TransformControls } from '@react-three/drei';
+import { useRef } from 'react';
+import { Mesh } from 'three';
 
 export default function Experience() {
+  const cube = useRef<Mesh>(null);
+
   return (
     <>
-      <OrbitControls enableDamping={false} />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
+        scale={100}
+        fixed={true}
+      >
+        <mesh position-x={-2} scale={2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </PivotControls>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+
+      <TransformControls object={cube} mode="translate" />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
