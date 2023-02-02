2261265eada17aae7ae7da675187394dc55dd764
r-shirasu <risako.shirasu@asia-quest.jp>
2023-02-03 00:19:33 +0900

    feat: show draco

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,21 +1,32 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from '@react-three/drei';
+import { Perf } from 'r3f-perf';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { useLoader } from '@react-three/fiber';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const model = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('./draco/');
+    loader.setDRACOLoader(dracoLoader);
+  });
 
-        <Perf position="top-left" />
+  console.info(model);
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+      <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
 
+      <primitive object={model.scene} scale={0.35} />
     </>
+  );
 }
