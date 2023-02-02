73279033941d8e1996877af65c6fcb4aed61e5ab
r-shirasu <risako.shirasu@asia-quest.jp>
2023-02-03 01:19:19 +0900

    feat: アニメーションまで

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,7 +2,9 @@ import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 import Model from './Model.jsx';
 import { Suspense } from 'react';
-import Placeholder from './Placeholder.jsx';
+import { Hamburger } from './Hamburger';
+import Placeholder from './Placeholder';
+import Fox from './Fox';
 
 export default function Experience() {
   return (
@@ -11,7 +13,7 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
       <ambientLight intensity={0.5} />
 
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
@@ -20,8 +22,10 @@ export default function Experience() {
       </mesh>
 
       <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
-        <Model />
+        <Hamburger scale={0.35} />
       </Suspense>
+
+      <Fox />
     </>
   );
 }
--- src/Model.tsx
+++ src/Model.tsx
@@ -1,13 +1,13 @@
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useLoader } from '@react-three/fiber';
+import { Clone, useGLTF } from '@react-three/drei';
 
 export default function Model() {
-  const model = useLoader(GLTFLoader, './FlightHelmet/glTF/FlightHelmet.gltf', (loader) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('./draco/');
-    loader.setDRACOLoader(dracoLoader);
-  });
+  const model = useGLTF('./hamburger-draco.glb');
 
-  return <primitive object={model.scene} scale={5} position-y={-1} />;
+  return (
+    <>
+      <Clone object={model.scene} scale={0.35} position-x={-4} />
+      <Clone object={model.scene} scale={0.35} position-x={0} />
+      <Clone object={model.scene} scale={0.35} position-x={4} />
+    </>
+  );
 }
