c536ab067efa890dbce5e5be99402b8e96bef825
r-shirasu <risako.shirasu@asia-quest.jp>
2023-02-03 00:12:52 +0900

    feat: リセット

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,50 +1,21 @@
-import { OrbitControls } from '@react-three/drei';
-import { useControls, button } from 'leva';
-import { Perf } from 'r3f-perf';
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
 
-export default function Experience() {
-  const { position, color, visible, perfVisible } = useControls('sphere', {
-    position: {
-      value: { x: -2, y: 0 },
-      step: 0.01,
-      joystick: 'invertY',
-    },
-    color: '#ff0000',
-    visible: true,
-    myInterval: {
-      min: 0,
-      max: 10,
-      value: [4, 5],
-    },
-    clickMe: button(() => {
-      console.info('ok');
-    }),
-    choice: { options: ['a', 'b', 'c'] },
-    perfVisible: true,
-  });
+export default function Experience()
+{
+    return <>
 
-  return (
-    <>
-      {perfVisible && <Perf position="top-left" />}
-      <OrbitControls makeDefault />
+        <Perf position="top-left" />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+        <OrbitControls makeDefault />
 
-      <mesh visible={visible} position={[position.x, position.y, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color={color} />
-      </mesh>
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-      <mesh position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
     </>
-  );
 }
