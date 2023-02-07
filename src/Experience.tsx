4957adef426fd1bdaaf994939bad9c03403ba66e
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-08 00:41:58 +0900

    feat: 複数のドーナツを生成する

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,7 +1,9 @@
-import { Text3D, OrbitControls, Center } from '@react-three/drei';
+import { Text3D, OrbitControls, Center, useMatcapTexture } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
+  const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+
   return (
     <>
       <Perf position="top-left" />
@@ -21,9 +23,20 @@ export default function Experience() {
           bevelSegments={5}
         >
           HELLO R3F
-          <meshNormalMaterial />
         </Text3D>
       </Center>
+
+      {[...Array(100)].map((value, index) => (
+        <mesh
+          key={index}
+          position={[(Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+        >
+          <torusGeometry args={[1, 0.6, 16, 32]} />
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </mesh>
+      ))}
     </>
   );
 }
