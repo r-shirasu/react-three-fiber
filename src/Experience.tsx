2832d3797a9c3dec1e634dad2d38cadce499d0ac
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-08 01:18:50 +0900

    feat: ドーナツのアニメーション

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,37 +1,41 @@
-import { Text3D, OrbitControls, Center, useMatcapTexture } from '@react-three/drei';
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
-import { useState } from 'react';
-import { TorusGeometry } from 'three';
+import { useEffect, useRef} from 'react';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
+import { BufferGeometry, Material, Mesh } from 'three';
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const donuts = useRef<Mesh<BufferGeometry, Material | Material[]> | any>([]);
+
   const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
-  const [torusGeometry, setTorusGeometry] = useState();
-  const [material, setMaterial] = useState();
+
+  useFrame((state, delta) => {
+    for (const donut of donuts.current) {
+      if (!donut) {
+        return;
+      }
+      donut.rotation.y += delta * 0.2;
+    }
+  });
+
+  useEffect(() => {
+    matcapTexture.encoding = THREE.sRGBEncoding;
+    matcapTexture.needsUpdate = true;
+
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, [matcapTexture]);
 
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
-
-      <Center>
-        <Text3D
-          font="./fonts/helvetiker_regular.typeface.json"
-          size={0.75}
-          height={0.2}
-          curveSegments={12}
-          bevelEnabled
-          bevelThickness={0.02}
-          bevelSize={0.02}
-          bevelOffset={0}
-          bevelSegments={5}
-        >
-          HELLO R3F
-        </Text3D>
-      </Center>
-
-      <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
-      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
 
       <Center>
         <Text3D
@@ -52,13 +56,16 @@ export default function Experience() {
 
       {[...Array(100)].map((value, index) => (
         <mesh
+          ref={(element) => (donuts.current[index] = element)}
           key={index}
           geometry={torusGeometry}
           material={material}
           position={[(Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]}
           scale={0.2 + Math.random() * 0.2}
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        />
+        >
+          <torusGeometry args={[1, 0.6, 16, 32]} />
+        </mesh>
       ))}
     </>
   );
