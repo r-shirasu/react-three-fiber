60c24a9b1a4324b36fc0005562b3a5f93c18541d
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-08 00:48:24 +0900

    feat: 1つのマテリアルのみを使用する

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,8 +1,12 @@
 import { Text3D, OrbitControls, Center, useMatcapTexture } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
+import { useState } from 'react';
+import { TorusGeometry } from 'three';
 
 export default function Experience() {
   const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+  const [torusGeometry, setTorusGeometry] = useState();
+  const [material, setMaterial] = useState();
 
   return (
     <>
@@ -26,16 +30,35 @@ export default function Experience() {
         </Text3D>
       </Center>
 
+      <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+
+      <Center>
+        <Text3D
+          material={material}
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          HELLO R3F
+        </Text3D>
+      </Center>
+
       {[...Array(100)].map((value, index) => (
         <mesh
           key={index}
+          geometry={torusGeometry}
+          material={material}
           position={[(Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]}
           scale={0.2 + Math.random() * 0.2}
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        >
-          <torusGeometry args={[1, 0.6, 16, 32]} />
-          <meshMatcapMaterial matcap={matcapTexture} />
-        </mesh>
+        />
       ))}
     </>
   );
