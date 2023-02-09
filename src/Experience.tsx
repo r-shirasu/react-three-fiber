9a2c6f385d1c9e1b708037c0daa6a9a481b82f0e
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-10 01:04:56 +0900

    feat: アニメーションまで

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,6 +1,11 @@
-import { Center, OrbitControls, Sparkles, useGLTF, useTexture } from '@react-three/drei';
+import { Center, OrbitControls, shaderMaterial, Sparkles, useGLTF, useTexture } from '@react-three/drei';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
+import portalVertexShader from './shaders/portal/vertex.glsl';
+import portalFragmentShader from './shaders/portal/fragment.glsl';
+import { extend, ReactThreeFiber, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { ShaderMaterial } from 'three';
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -10,15 +15,42 @@ type GLTFResult = GLTF & {
     portalLight: THREE.Mesh;
   };
 };
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color('#ffffff'),
+    uColorEnd: new THREE.Color('#000000'),
+  },
+  portalVertexShader,
+  portalFragmentShader,
+);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      portalMaterial: ReactThreeFiber.Object3DNode<ShaderMaterial, typeof ShaderMaterial>;
+    }
+  }
+}
+
+extend({ PortalMaterial });
 export default function Experience() {
   const { nodes } = useGLTF('./model/portal.glb') as unknown as GLTFResult;
   const bakedTexture = useTexture('./model/baked.jpg');
   bakedTexture.flipY = false;
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const portalMaterial = useRef<ShaderMaterial & { uTime: number }>(null!);
+
+  useFrame((state, delta) => {
+    portalMaterial.current.uTime += delta;
+  });
+
   return (
     <>
       <color args={['#030202']} attach="background" />
-
       <OrbitControls makeDefault />
 
       <Center>
@@ -39,7 +71,7 @@ export default function Experience() {
           position={nodes.portalLight.position}
           rotation={nodes.portalLight.rotation}
         >
-          <meshBasicMaterial color="#ffffff" />
+          <portalMaterial ref={portalMaterial} />
         </mesh>
 
         <Sparkles size={6} scale={[4, 2, 4]} position-y={1} speed={0.2} count={40} />
