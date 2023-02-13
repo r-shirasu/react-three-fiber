71351b403bfdd4f136b49f954e9083960090988b
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-14 01:15:22 +0900

    feat: ブルームまで

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,47 +1,37 @@
 import { OrbitControls } from '@react-three/drei';
-import { useControls, button } from 'leva';
 import { Perf } from 'r3f-perf';
+import { Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
 
 export default function Experience() {
-  const { position, color, visible, perfVisible } = useControls('sphere', {
-    position: {
-      value: { x: -2, y: 0 },
-      step: 0.01,
-      joystick: 'invertY',
-    },
-    color: '#ff0000',
-    visible: true,
-    myInterval: {
-      min: 0,
-      max: 10,
-      value: [4, 5],
-    },
-    clickMe: button(() => {
-      console.info('ok');
-    }),
-    choice: { options: ['a', 'b', 'c'] },
-    perfVisible: true,
-  });
-
   return (
     <>
-      {perfVisible && <Perf position="top-left" />}
+      <color args={['#ffffff']} attach="background" />
+
+      <EffectComposer multisampling={4}>
+        <Vignette offset={0.3} darkness={0.9} blendFunction={BlendFunction.NORMAL} />
+        <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} />
+        <Bloom mipmapBlur intensity={0.5} luminanceThreshold={0} />
+      </EffectComposer>
+
+      <Perf position="top-left" />
+
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh visible={visible} position={[position.x, position.y, 0]}>
+      <mesh castShadow position-x={-2}>
         <sphereGeometry />
-        <meshStandardMaterial color={color} />
+        <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
