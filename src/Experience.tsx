99269a0f8394b7141f425acdb0372f3c42aaa1c6
r-shirasu <fortechacademy.risako1011@gmail.com>
2023-02-16 00:24:08 +0900

    feat: ラップトップの表示

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,15 +1,19 @@
-import { OrbitControls } from '@react-three/drei';
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
 
 export default function Experience() {
+  const computer = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf',
+  );
 
   return (
     <>
+      <Environment preset="city" />
+
+      <color args={['#241a1a']} attach="background" />
+
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <primitive object={computer.scene} />
     </>
   );
 }
